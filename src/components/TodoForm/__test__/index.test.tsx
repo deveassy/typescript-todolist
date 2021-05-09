d5ff07192e3f8b6e7a9a39f5d246b50d58a6b4be
deveassy <xjfmwld@gmail.com>
2021-05-09 18:48:23 +0900

    TodoForm tsx, test 작업

--- src/components/TodoForm/__test__/index.test.tsx
+++ src/components/TodoForm/__test__/index.test.tsx
@@ -2,4 +2,37 @@ import React from "react";
 import { fireEvent, render } from "@testing-library/react";
 import TodoForm from "../";
 
-describe("<TodoForm />", () => {});
+describe("<TodoForm />", () => {
+  it("has input and a button", () => {
+    const { getByText, getByPlaceholderText } = render(<TodoForm />);
+    getByPlaceholderText("할 일을 입력하세요");
+    getByText("추가");
+  });
+  it("has input changing", () => {
+    const { getByPlaceholderText } = render(<TodoForm />);
+    const input = getByPlaceholderText("할 일을 입력하세요");
+    fireEvent.change(input, {
+      target: {
+        value: "타입스크립트 적용중",
+      },
+    });
+    expect(input).toHaveAttribute("value", "타입스크립트 적용중");
+  });
+  it("calls onInsert and when button clicked, clears input", () => {
+    const onInsert = jest.fn();
+    const { getByPlaceholderText, getByText } = render(
+      <TodoForm onInsert={onInsert} />
+    );
+    const input = getByPlaceholderText("할 일을 입력하세요");
+    const button = getByText("추가");
+
+    fireEvent.change(input, {
+      target: {
+        value: "타입스크립트 적용중",
+      },
+    });
+    fireEvent.click(button);
+    expect(onInsert).toBeCalledWith("타입스크립트 적용중");
+    expect(input).toHaveAttribute("value", "");
+  });
+});
--- src/components/TodoForm/index.tsx
+++ src/components/TodoForm/index.tsx
@@ -1,7 +1,29 @@
 import React, { useState } from "react";
 
-function TodoForm() {
-  return <div />;
+function TodoForm(props: any) {
+  const { onInsert } = props;
+  const [value, setValue] = useState("");
+
+  const handleChange = (e: any) => {
+    setValue(e.target.value);
+  };
+
+  const handleSubmit = (e: any) => {
+    onInsert(value);
+    setValue("");
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        value={value}
+        onChange={handleChange}
+        placeholder="할 일을 입력하세요"
+      />
+      <button>추가</button>
+    </form>
+  );
 }
 
 export default TodoForm;
